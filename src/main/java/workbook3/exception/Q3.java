ca0cd0a935836306304e971b0ca18524f8329f1c
jinhee kim <lsa68803@naver.com>
2025-09-03 18:34:51 +0900

    feat: [exception] Q3 구현

--- src/main/java/workbook3/exception/Q3.java
+++ src/main/java/workbook3/exception/Q3.java
@@ -8,8 +8,18 @@ public class Q3 {
         String s = sc.next();
         try {
             // TODO: s를 int로 변환 후 2 곱해 출력
+            System.out.println(Integer.parseInt(s) * 2);
         } catch (NumberFormatException e) {
             System.out.println("Invalid number");
         }
     }
 }
+
+/*
+    [ Integer.parseInt(s) ]
+
+       - 문자열을 int로 변환
+       - 이 때, 모든 문자열이 아닌 숫자 형태의 문자열만 int로 변환
+       - 숫자 형태가 아닌 문자열 경우 "예외" 구문으로 빠짐
+
+ */
