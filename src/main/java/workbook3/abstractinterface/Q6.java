02018b2088eb9d49fee2418553d7c3af07d4efb9
jinhee kim <lsa68803@naver.com>
2025-09-03 18:18:56 +0900

    feat: [abstractinterface] Q6 구현

--- src/main/java/workbook3/abstractinterface/Q6.java
+++ src/main/java/workbook3/abstractinterface/Q6.java
@@ -13,16 +13,19 @@ interface Rotatable {
 class Square implements Drawable, Rotatable {
     public void draw() {
         // TODO: "Square drawn" 출력
+        System.out.println("Square drawn");
     }
 
     public void rotate() {
         // TODO: "Square rotated" 출력
+        System.out.println("Square rotated");
     }
 }
 
 class Circle implements Drawable {
     public void draw() {
         // TODO: "Circle drawn" 출력
+        System.out.println("Circle drawn");
     }
 }
 
@@ -36,10 +39,15 @@ public class Q6 {
         d1.draw();
         if (d1 instanceof Rotatable) { // d1이 Rotatable 인터페이스(또는 그 하위 타입)를 실제로 구현한 객체인지 확인
             // TODO: d1 객체를 활용하여 rotate() 메서드를 호출한다.
+            ((Rotatable) d1).rotate();
+            // d1 타입 : Drawable  -> rotate() 메서드 바로 호출 불가
+            // " (Rotatable) d1 " -> Rotatable 인터페이스로 캐스팅 필요!
+            // 타입 변환 후 rotate() 메서드 호출
         }
         d2.draw();
         if (d2 instanceof Rotatable) { // d2이 Rotatable 인터페이스(또는 그 하위 타입)를 실제로 구현한 객체인지 확인
             // TODO: d2 객체를 활용하여 rotate() 메서드를 호출한다.
+            ((Rotatable) d2).rotate();
         }
     }
 }
